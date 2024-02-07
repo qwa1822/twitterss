0089989162f0fe47a9a487ed2d568a340433bdcf
qwa1822 <asdzxc1822@naver.com>
2024-02-07 18:24:57 +0900

    'react-router-페이지설정'

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
+import Router from "Router";
+import HomePage from "pages/home";
 import { Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<h1>홈페이지</h1>} />
-      <Route path="/posts" element={<h1>postsList</h1>} />
-      <Route path="/posts/:id" element={<h1>Detail페이지</h1>} />
-      <Route path="/posts/new" element={<h1>New페이지</h1>} />
-      <Route path="/posts/edit/:id" element={<h1>12331312Edit</h1>} />
-      <Route path="/profile" element={<h1>Profile</h1>} />
-      <Route path="/profile/edit" element={<h1>Profile Edit</h1>} />
-      <Route path="/notification" element={<h1>notifiaction1</h1>} />
-      <Route path="/search" element={<h1>search</h1>} />
-      <Route path="/users/login" element={<h1>login</h1>} />
-      <Route path="/users/signup" element={<h1>SignUp</h1>} />
-      <Route path="*" element={<Navigate replace to="/" />} />
-    </Routes>
-  );
+  return <Router />;
 }
 
 export default App;
---
+++ src/pages/users/UserSign.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function UserSign() {
+  return <div>UserSign</div>;
+}
